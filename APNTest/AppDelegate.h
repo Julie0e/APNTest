5be93c4eaee470f2a81a542a0b4074e2616984e4
Julie0e <julie_91@hanmail.net>
2014-01-20 14:18:58 +0900

    Initial Commit

---
+++ APNTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APNTest
+//
+//  Created by 주리 on 2014. 1. 20..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
