8d31683c1ad1002e8f65e466b6e34b53ae837aed
Julie0e <julie_91@hanmail.net>
2014-01-20 17:50:38 +0900

    39page

--- APNTest/AppDelegate.m
+++ APNTest/AppDelegate.m
@@ -5,16 +5,53 @@
 //  Created by 주리 on 2014. 1. 20..
 //  Copyright (c) 2014년 T. All rights reserved.
 //
-
 #import "AppDelegate.h"
+#import <AudioToolbox/AudioToolbox.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    
+//    UIRemoteNotificationType notiType = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+//    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notiType];
+//  
+//    // Override point for customization after application launch.
     return YES;
 }
+
+ - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSLog(@"Success to register APN : %@", deviceToken);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"Fail to register APN : %@", [error localizedDescription]);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"remote notification: %@", [userInfo description]);
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    
+    NSString *alert = [apsInfo objectForKey:@"alert"];
+    NSLog(@"Received Push Alert : %@", alert);
+    
+    NSString *sound = [apsInfo objectForKey:@"sound"];
+    NSLog(@"Received Push Sound : %@", sound);
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+    NSString *badge = [apsInfo objectForKey:@"badge"];
+    NSLog(@"Received Push Badge : %@", badge);
+    application.applicationIconBadgeNumber = [[apsInfo objectForKey:@"badge"]integerValue];
+    
+    UIAlertView *alter2 = [[UIAlertView alloc] initWithTitle:@"aa" message:alert delegate:self cancelButtonTitle:nil otherButtonTitles:@"확인", nil];
+    [alter2 show];
+}
+
+
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
